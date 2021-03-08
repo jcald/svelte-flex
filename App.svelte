c3a65620bc9c01520eb63289979d5f2064baee59
José Calderón-Celis <josecalderoncelis@gmail.com>
2021-03-08 09:20:45 -0500

    Initial commit

---
+++ App.svelte
@@ -0,0 +1,52 @@
+<script>
+  import Flex from "svelte-flex";
+  import Box from "./Box.svelte";
+</script>
+
+<style>
+  main {
+    font-family: sans-serif;
+    text-align: center;
+    margin: 1rem auto;
+    min-width: 420px;
+    width: 50%;
+  }
+  hr {
+    margin: 1rem auto;
+    border: 0;
+    border-bottom: 2px dotted #333;
+  }
+</style>
+
+<main>
+  <h1>svelte-flex</h1>
+  <p>Play around with some <a href="https://github.com/himynameisdave/svelte-flex" target="_blank">svelte-flex</a> examples.</p>
+  <hr />
+  <h2>Default:</h2>
+	<Flex>
+    <Box color="lime" />
+    <Box color="blue" />
+    <Box color="red" />
+	</Flex>
+  <hr />
+  <h2>direction="column"</h2>
+  <Flex direction="column">
+    <Box color="lime" />
+    <Box color="blue" />
+    <Box color="red" />
+	</Flex>
+  <hr />
+  <h2>align="start"</h2>
+  <Flex align="stretch">
+    <Box color="lime" />
+    <Box color="blue" />
+    <Box color="red" />
+	</Flex>
+  <hr />
+  <h2>justify="end"</h2>
+  <Flex justify="end">
+    <Box color="lime" />
+    <Box color="blue" />
+    <Box color="red" />
+	</Flex>
+</main>
---
+++ Box.svelte
@@ -0,0 +1,25 @@
+<script>
+  export let color = "lime";
+</script>
+
+<style>
+  div {
+    border: 2px dotted #333;
+    height: 60px;
+    width: 60px;
+  }
+  .lime {
+    background: lime;
+  }
+  .blue {
+    background: #00aacc;
+    height: 80px;
+  }
+  .red {
+    background: #ff6666;
+  }
+</style>
+
+<div class={color}>
+  {color}
+</div>
